d2287652b8bb7536db50cf602aba28c01ea05725
Amith Acharya <amith99achar@gmail.com>
2026-04-04 18:27:46 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,28 +1,19 @@
-############################
-# GKE Cluster Output
-############################
 output "cluster_id" {
   value = google_container_cluster.privategpt.id
 }
 
-############################
-# Node Pool Output
-############################
-output "node_pool_id" {
-  value = google_container_node_pool.privategpt_nodes.id
+output "cluster_name" {
+  value = google_container_cluster.privategpt.name
 }
 
-############################
-# VPC Network Output
-############################
+output "cluster_endpoint" {
+  value = google_container_cluster.privategpt.endpoint
+}
+
 output "vpc_id" {
   value = google_compute_network.privategpt_vpc.id
 }
 
-############################
-# Subnet Output
-############################
 output "subnet_id" {
-  value = google_compute_subnetwork.privategpt_subnet[*].id
+  value = google_compute_subnetwork.privategpt_subnet.id
 }
-
