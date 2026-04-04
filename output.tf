4f6809ed2c0ec804e87fd27ee85831d153984283
Amith Acharya <amith99achar@gmail.com>
2026-04-04 23:22:36 +0530

    Refactor outputs for GKE cluster and subnet
    Updated outputs for GKE cluster and node pool, and modified subnet output to return a list of IDs.

--- output.tf
+++ output.tf
@@ -1,19 +1,27 @@
+############################
+# GKE Cluster Output
+############################
 output "cluster_id" {
   value = google_container_cluster.privategpt.id
 }
 
-output "cluster_name" {
-  value = google_container_cluster.privategpt.name
+############################
+# Node Pool Output
+############################
+output "node_pool_id" {
+  value = google_container_node_pool.privategpt_nodes.id
 }
 
-output "cluster_endpoint" {
-  value = google_container_cluster.privategpt.endpoint
-}
-
+############################
+# VPC Network Output
+############################
 output "vpc_id" {
   value = google_compute_network.privategpt_vpc.id
 }
 
+############################
+# Subnet Output
+############################
 output "subnet_id" {
-  value = google_compute_subnetwork.privategpt_subnet.id
+  value = google_compute_subnetwork.privategpt_subnet[*].id
 }
